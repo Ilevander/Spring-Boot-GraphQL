9ef3cbf70513c23fe20298b171448572f39145ea
dell <dell@DESKTOP-4P8U8AR>
2024-12-25 23:18:58 +0100

    initilizing project including main dependencies

---
+++ src/main/java/com/ilyass/bankwebservicespringbootgraphql/BankWebServiceSpringBootGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.ilyass.bankwebservicespringbootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebServiceSpringBootGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankWebServiceSpringBootGraphQlApplication.class, args);
+    }
+
+}
