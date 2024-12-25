b0236a91e56669c3df0355083039cbcd0cf4104d
dell <dell@DESKTOP-4P8U8AR>
2024-12-26 00:48:32 +0100

    service/exception/BusinessException

---
+++ src/main/java/com/ilyass/bankwebservicespringbootgraphql/service/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.ilyass.bankwebservicespringbootgraphql.service.exception;
+
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
